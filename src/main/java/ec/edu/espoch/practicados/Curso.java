83a5e9e02ccd690cdd9bb311571981105455d177
GlendaAlvarado <alvaradosalazarglenda@gmail.com>
2025-09-30 12:06:21 -0500

    hola

---
+++ src/main/java/ec/edu/espoch/practicados/Curso.java
@@ -0,0 +1,19 @@
+
+package ec.edu.espoch.practicados;
+import java.util.List;
+public class Curso {
+   //atributos
+   public String nombreCurso;
+   public int codigo;
+   public List<Estudiante> listaEstudiante ;
+   //metodos
+   public void agregarEstudiante(){
+       
+   }
+   public void eliminarEstudiante(){
+       
+   }
+   public void mostrarListaEstudiante(){
+       
+   }
+}
---
+++ src/main/java/ec/edu/espoch/practicados/Estudiante.java
@@ -0,0 +1,19 @@
+
+package ec.edu.espoch.practicados;
+
+public class Estudiante {
+    //atributos
+    public String nombre;
+    public String indentificacion;
+    public double promedio;
+    //metodos
+    public void incribirse(){
+        
+    }
+    public void retirarse(){
+        
+    }
+    public void  mostrarInformacion(){
+        
+    }
+}
